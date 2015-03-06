17723fdb0cdfdd2d8dbcf6f26ace7b67773638b5
Arush Bazaei <abguitar565@gmail.com>
2015-03-06 14:27:45 -0700

    Signed-off-by: Arush Bazaei <abguitar565@gmail.com>

---
+++ src/UI/MainMenu.java
@@ -0,0 +1,23 @@
+package UI;
+
+import java.awt.Color;
+
+import greenfoot.World;
+
+public class MainMenu extends World {
+
+	private static final int WIDTH = 500;
+	private static final int HEIGHT = 600;
+	private static final int CELL = 1;
+
+	public MainMenu() {
+		super(WIDTH,HEIGHT,CELL);
+		getBackground().setColor(Color.green);
+		getBackground().fill();
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hey");
+	}
+
+}
