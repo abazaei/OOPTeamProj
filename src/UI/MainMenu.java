64aa64a0daedc490e4c1912a768a872a3c78060e
Arush Bazaei <abguitar565@gmail.com>
2015-03-09 15:57:55 -0600

    No Greenfoot

--- src/UI/MainMenu.java
+++ src/UI/MainMenu.java
@@ -2,18 +2,16 @@ package UI;
 
 import java.awt.Color;
 
-import greenfoot.World;
+import javax.swing.JFrame;
 
-public class MainMenu extends World {
+
+
+public class MainMenu extends JFrame{
 
 	private static final int WIDTH = 500;
 	private static final int HEIGHT = 600;
-	private static final int CELL = 1;
 
 	public MainMenu() {
-		super(WIDTH,HEIGHT,CELL);
-		getBackground().setColor(Color.green);
-		getBackground().fill();
 		System.out.println("hello");
 		
 	}
