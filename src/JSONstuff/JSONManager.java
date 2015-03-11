473f3036423fee8e0fe9a07c4f50acd7de71642b
Arush Bazaei <abguitar565@gmail.com>
2015-03-11 16:22:08 -0600

    JSON Progress

--- src/JSONstuff/JSONManager.java
+++ src/JSONstuff/JSONManager.java
@@ -1,14 +1,42 @@
 package JSONstuff;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import com.google.gson.*;
 
 public class JSONManager{
 	
-	JsonParser parser = new JsonParser();
 	
+	BufferedReader breader;
+	
+	JsonParser parser = new JsonParser();
+	JsonElement root;
+	JsonObject rootarray;
+	JsonObject teams;
+	JsonObject teamname;
+	JsonObject player;
+	int age;
+	String reaction;
+	String overall;
 	
 	
 	public JSONManager(){
+		 try {
+			breader = new BufferedReader(new FileReader("TeamsAndPlayers.json"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		root = parser.parse(breader);
+		rootarray = root.getAsJsonObject(); //rootarray now = teams array
+		teams = rootarray.get("Teams").getAsJsonObject();
+		teamname = teams.get("FC Barcelona").getAsJsonObject();
+		player = teamname.getAsJsonObject("Stegen");
+		System.out.println(player.get("age").getAsString());
+		
+		
 		
 	}
 
