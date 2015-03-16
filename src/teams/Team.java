099558138b45e4f61cee6114f21a625405903457
Arush Bazaei <abguitar565@gmail.com>
2015-03-15 20:15:40 -0600

    Team Placeholders
    Have 3 Current Teams as place holders, try using those for the swing
    stuff and see what happens, the position I have in the Team.java is a
    string. Not a Position, I don't know how you wanted that.

--- src/teams/Team.java
+++ src/teams/Team.java
@@ -12,35 +12,49 @@ public class Team {
 	
 	static Player [] Barcelona = new Player [TEAMSIZE];
 	static Player [] RealMadrid = new Player [TEAMSIZE];
-
+	static Player [] PSG = new Player[TEAMSIZE];
 	
 	public Team()
 	{
 		//FC Barcelona
 		Barcelona[0] = new Player("Bravo", 83, 32, "Goalie","FC Barcelona", "Chile");
-		Barcelona[1] = new Player("Montoya", 81, 24, "Defender"); //FINISH AND ADD PSG, BAYERN,CHELSEA
-		Barcelona[2] = new Player("Pique", 87, 28, "Defender");
-		Barcelona[3] = new Player("Rakitic", 82, 26,"Midfielder");
-		Barcelona[4] = new Player("Busquets", 84, 26,"Midfielder");
-		Barcelona[5] = new Player("Xavi", 86, 34,"Midfielder");
-		Barcelona[6] = new Player("Pedro", 85, 27,"Forward");
-		Barcelona[7] = new Player("Iniesta", 89, 30,"Midfielder");
-		Barcelona[8] = new Player("Suarez", 92, 27,"Forward");
-		Barcelona[9] = new Player("Messi", 97, 27,"Forward");
-		Barcelona[10] = new Player("Neymar", 91, 22,"Forward");
+		Barcelona[1] = new Player("Montoya", 81, 24, "Defender", "FC Barcelona", "Spain"); //FINISH AND ADD PSG, BAYERN,CHELSEA
+		Barcelona[2] = new Player("Pique", 87, 28, "Defender", "FC Barcelona", "Spain");
+		Barcelona[3] = new Player("Rakitic", 82, 26,"Midfielder", "FC Barcelona", "Croatia");
+		Barcelona[4] = new Player("Busquets", 84, 26,"Midfielder", "FC Barcelona", "Spain");
+		Barcelona[5] = new Player("Xavi", 86, 34,"Midfielder", "FC Barcelona", "Spain");
+		Barcelona[6] = new Player("Pedro", 85, 27,"Forward", "FC Barcelona", "Spain");
+		Barcelona[7] = new Player("Iniesta", 89, 30,"Midfielder", "FC Barcelona", "Spain");
+		Barcelona[8] = new Player("Suarez", 92, 27,"Forward", "FC Barcelona", "Uruguay");
+		Barcelona[9] = new Player("Messi", 97, 27,"Forward", "FC Barcelona", "Argentina");
+		Barcelona[10] = new Player("Neymar", 91, 22,"Forward", "FC Barcelona", "Brazil");
 		
 		//Real Madrid
-		RealMadrid[0] = new Player("Casillas", 91, 33, "Goalie");
-		RealMadrid[1] = new Player("Varane", 83, 21, "Defender");
-		RealMadrid[2] = new Player("Pepe", 87, 31, "Defender");
-		RealMadrid[3] = new Player("Ramos", 86, 28, "Defender");
-		RealMadrid[4] = new Player("Coentrao", 83, 29, "Defender");
-		RealMadrid[5] = new Player("Khedira", 85, 27, "Midfielder");
-		RealMadrid[6] = new Player("Ronaldo", 97, 29, "Forward");
-		RealMadrid[7] = new Player("Kroos", 91, 24, "Midfielder");
-		RealMadrid[8] = new Player("Benzema", 89, 26, "Benzema");
-		RealMadrid[9] = new Player("Rodriguez", 86, 23, "Midfielder");
-		RealMadrid[10] = new Player("Bale", 89, 25, "Midfielder");	
+		RealMadrid[0] = new Player("Casillas", 84, 33, "Goalie", "FC Barcelona", "Spain");
+		RealMadrid[1] = new Player("Varane", 83, 21, "Defender", "FC Barcelona", "France");
+		RealMadrid[2] = new Player("Pepe", 87, 31, "Defender", "FC Barcelona", "Portugal");
+		RealMadrid[3] = new Player("Ramos", 86, 28, "Defender", "FC Barcelona", "Spain");
+		RealMadrid[4] = new Player("Coentrao", 83, 29, "Defender", "FC Barcelona", "Portugal");
+		RealMadrid[5] = new Player("Khedira", 85, 27, "Midfielder", "FC Barcelona", "Germany");
+		RealMadrid[6] = new Player("Ronaldo", 97, 29, "Forward", "FC Barcelona", "Portugal");
+		RealMadrid[7] = new Player("Kroos", 91, 24, "Midfielder", "FC Barcelona", "Germany");
+		RealMadrid[8] = new Player("Benzema", 89, 26, "Forward", "FC Barcelona", "France");
+		RealMadrid[9] = new Player("Rodriguez", 86, 23, "Midfielder", "FC Barcelona", "Spain");
+		RealMadrid[10] = new Player("Bale", 89, 25, "Midfielder", "FC Barcelona", "Wale");
+		
+		//PSG
+		PSG[0] = new Player("Sirigu",84,28,"Goalie","Paris Saint-Germain F.C.","Italy");
+		PSG[1] = new Player("Silva",91,31,"Defender","Paris Saint-Germain F.C.","Brazil");
+		PSG[2] = new Player("Luiz",89,28,"Defender","Paris Saint-Germain F.C.","Brazil");
+		PSG[3] = new Player("Aurier",81,27,"Defender","Paris Saint-Germain F.C.","Ivory Coast");
+		PSG[4] = new Player("Maxwell",79,34,"Midfielder","Paris Saint-Germain F.C.","Brazil");
+		PSG[5] = new Player("Motta",82,33,"Midfielder","Paris Saint-Germain F.C.","Italy");
+		PSG[6] = new Player("Matuidi",83,28,"Midfielder","Paris Saint-Germain F.C.","France");
+		PSG[7] = new Player("Veratti",80,23,"Midfielder","Paris Saint-Germain F.C.","Italy");
+		PSG[8] = new Player("Lavezzi",82,30,"Forward","Paris Saint-Germain F.C.","Argentina");
+		PSG[9] = new Player("Ibrahimovic",91,34,"Forward","Paris Saint-Germain F.C.","Sweden");
+		PSG[10] = new Player("Cavani",86,28,"Forward","Paris Saint-Germain F.C.","Uruguay");
+	}
 
 		
 
