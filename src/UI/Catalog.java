8bcf11d0c664eab79a682739458e49dba5d376fe
Kurtis Lloyd <kurtyboyfloyd@gmail.com>
2015-03-15 20:55:51 -0600

    buttons

--- src/UI/Catalog.java
+++ src/UI/Catalog.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -38,20 +39,63 @@ public class Catalog extends JPanel {
 		teamBox.setBounds(180,70,100,45);
 		teams.setBounds(100,70,100,45);
 		players.setBounds(100,150,100,45);
+		teamBox.addActionListener(new ComboBoxListener());
 		
 
 	}
 
 
 	public class ComboBoxListener implements ActionListener {
-
+		
 
 
 
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
+			JButton button1 = new JButton();
+			JButton button2 = new JButton();
+			JButton button3 = new JButton();
+			JButton button4 = new JButton();
+			JButton button5 = new JButton();
+			JButton button6 = new JButton();
+			JButton button7 = new JButton();
+			JButton button8 = new JButton();
+			JButton button9 = new JButton();
+			JButton button10 = new JButton();
+			JButton button11 = new JButton();
 			
+			
+			add(button1);
+			add(button2);
+			add(button3);
+			add(button4);
+			add(button5);
+			add(button6);
+			add(button7);
+			add(button8);
+			add(button9);
+			add(button10);
+			add(button11);
+			
+			button1.setBounds(150, 150,100,30);
+			button2.setBounds(150, 190,100,30);
+			button3.setBounds(150,240,100,30);
+			button4.setBounds(150, 280,100,30);
+			button5.setBounds(150, 320,100,30);
+			button6.setBounds(150, 360,100,30);
+			button7.setBounds(150, 400,100,30);
+			button8.setBounds(150, 440,100,30);
+			button9.setBounds(150, 480,100,30);
+			button10.setBounds(150, 520,100,30);
+			button11.setBounds(150, 560,100,30);
+			
+			
+			
+			
+			
+				
+			}
 		}
 
 
@@ -59,4 +103,4 @@ public class Catalog extends JPanel {
 	}
 
 
-}
+
